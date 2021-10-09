8e9f83792044622a011948576f51e6ae7e83e25e
Hugo Lundin <hugo@lundin.dev>
2021-10-09 18:40:55 +0200

    Add setup command for flashing Pico firmware

--- awg/main.py
+++ awg/main.py
@@ -1,10 +1,31 @@
+import os
+import tempfile
 import argparse
+import logging
+import coloredlogs
 
+RPI_PICO = '/dev/tty.usbmodem14601'
+FIRMWARE_PATH = 'awg/firmware.py'
 
 def setup():
-    pass
+    logging.info(f'Copying firmware to Raspberry Pi Pico ({RPI_PICO})...')
+    os.system(f'ampy -p {RPI_PICO} put {FIRMWARE_PATH} main.py')
 
+def main():
+    logging.debug('Starting awg...')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--setup', action='store_true')
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
 
+    if args.debug:
+        coloredlogs.install('DEBUG')
+    else:
+        coloredlogs.install('INFO')
+    
+    if args.setup:
+        setup()
+    else:
+        main()
--- main.py
+++ main.py
@@ -5,4 +5,4 @@ led_onboard = machine.Pin(25, machine.Pin.OUT)
 
 while True:
     led_onboard.toggle()
-    utime.sleep(1)
+    utime.sleep(0.1)
