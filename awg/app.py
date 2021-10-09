0ae8643e8b2854866897150a00df91b7e11578da
Hugo Lundin <hugo@lundin.dev>
2021-10-10 00:13:51 +0200

    Start interpreting expressions

--- awg/app.py
+++ awg/app.py
@@ -1,5 +1,6 @@
 import tkinter
 import numpy
+import inspect
 
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
@@ -15,18 +16,34 @@ class ArbitraryWaveGenerator:
         self.figure = Figure(figsize=(5, 4), dpi=100)
         t = numpy.arange(0, 3, .01)
         self.figure.add_subplot(111).plot(t, 2 * numpy.sin(2 * numpy.pi * t))
-        
         self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
         self.canvas.draw()
-        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
-        self.toolbar = NavigationToolbar2Tk(self.canvas, self.root)
-        self.toolbar.update()
-        self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-        self.canvas.mpl_connect("key_press_event", lambda event: key_press_handler(event, self.canvas, self.toolbar))
+        label = tkinter.Label(self.root, text='Function')
+        text = tkinter.StringVar()
+        text.trace('w', lambda name, index, mode, text=text: self.on_change(text))
+        field = tkinter.Entry(self.root, textvariable=text, width=40)
+        
+        self.canvas.get_tk_widget().grid(row=0, columnspan=2)
+        label.grid(row=1, column=0)
+        field.grid(row=1, column=1)
+        
 
-        self.frame.pack()
-        self.root.tk.call('tk', 'scaling', 2.0)
+        # self.figure = Figure(figsize=(5, 4), dpi=100)
+        # t = numpy.arange(0, 3, .01)
+        # self.figure.add_subplot(111).plot(t, 2 * numpy.sin(2 * numpy.pi * t))
+        
+        # self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
+        # self.canvas.draw()
+        # self.canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+        
+    def on_change(self, text):
+        
+        t = numpy.arange(0, 3, .01)
+        
+        self.figure.clear()
+        self.figure.add_subplot(111).plot(t, eval(text.get()))
+        self.canvas.draw_idle()
 
     def run(self):
         self.root.mainloop()
